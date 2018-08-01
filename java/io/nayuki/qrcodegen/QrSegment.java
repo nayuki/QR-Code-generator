4ac3973d0e139c81fc470da66ed2fbb5646883c7
Guillermo Rodríguez <guille.rodriguez@gmail.com>
2018-08-01 09:53:55 +0200

    Remove unused imports rather than commenting them out.

--- java/io/nayuki/qrcodegen/QrSegment.java
+++ java/io/nayuki/qrcodegen/QrSegment.java
@@ -23,10 +23,8 @@
 
 package io.nayuki.qrcodegen;
 
-//import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-//import java.util.Objects;
 import java.util.regex.Pattern;
 
 
