fd849b188cf78c78b9ec36876add6bf47a82b8e6
Project Nayuki <me@nayuki.io>
2020-03-05 17:46:11 +0000

    Updated package version numbers.

--- python/setup.py
+++ python/setup.py
@@ -27,7 +27,7 @@ import setuptools
 setuptools.setup(
 	name = "qrcodegen",
 	description = "High quality QR Code generator library for Python 2 and 3",
-	version = "1.5.0",
+	version = "1.6.0",
 	platforms = "OS Independent",
 	license = "MIT License",
 	
