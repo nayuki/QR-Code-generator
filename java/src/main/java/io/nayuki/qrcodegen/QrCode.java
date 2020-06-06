fe6c3980afb29637e981a6d3185709e0a0c790f9
gerzees <rudwls0511@pusan.ac.kr>
2020-06-06 18:01:16 +0900

    1. Extract class 2. Ecc from QrCode 3. QrCode is too big

--- java/src/main/java/io/nayuki/qrcodegen/QrCode.java
+++ java/src/main/java/io/nayuki/qrcodegen/QrCode.java
@@ -421,7 +421,7 @@ public final class QrCode {
 	// based on the given mask and this object's error correction level field.
 	private void drawFormatBits(int msk) {
 		// Calculate error correction code and pack bits
-		int data = errorCorrectionLevel.formatBits << 3 | msk;  // errCorrLvl is uint2, mask is uint3
+		int data = errorCorrectionLevel.getFormatBits() << 3 | msk;  // errCorrLvl is uint2, mask is uint3
 		int rem = data;
 		for (int i = 0; i < 10; i++)
 			rem = (rem << 1) ^ ((rem >>> 9) * 0x537);
@@ -512,8 +512,8 @@ public final class QrCode {
 			throw new IllegalArgumentException();
 		
 		// Calculate parameter numbers
-		int numBlocks = NUM_ERROR_CORRECTION_BLOCKS[errorCorrectionLevel.ordinal()][version];
-		int blockEccLen = ECC_CODEWORDS_PER_BLOCK  [errorCorrectionLevel.ordinal()][version];
+		int numBlocks = errorCorrectionLevel.getNumberOfBlock(version);
+		int blockEccLen = errorCorrectionLevel.getBlockLength(version);
 		int rawCodewords = getNumRawDataModules(version) / 8;
 		int numShortBlocks = numBlocks - rawCodewords % numBlocks;
 		int shortBlockLen = rawCodewords / numBlocks;
@@ -830,9 +830,9 @@ public final class QrCode {
 	// QR Code of the given version number and error correction level, with remainder bits discarded.
 	// This stateless pure function could be implemented as a (40*4)-cell lookup table.
 	static int getNumDataCodewords(int ver, Ecc ecl) {
-		return getNumRawDataModules(ver) / 8
-				- ECC_CODEWORDS_PER_BLOCK    [ecl.ordinal()][ver]
-						* NUM_ERROR_CORRECTION_BLOCKS[ecl.ordinal()][ver];
+		return getNumRawDataModules(ver) / 8 
+				- ecl.getBlockLength(ver) 
+				* ecl.getNumberOfBlock(ver);
 	}
 
 
@@ -888,48 +888,5 @@ public final class QrCode {
 	private static final int PENALTY_N2 =  3;
 	private static final int PENALTY_N3 = 40;
 	private static final int PENALTY_N4 = 10;
-
-
-	private static final byte[][] ECC_CODEWORDS_PER_BLOCK = {
-			// Version: (note that index 0 is for padding, and is set to an illegal value)
-			//0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40    Error correction level
-			{-1,  7, 10, 15, 20, 26, 18, 20, 24, 30, 18, 20, 24, 26, 30, 22, 24, 28, 30, 28, 28, 28, 28, 30, 30, 26, 28, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30},  // Low
-			{-1, 10, 16, 26, 18, 24, 16, 18, 22, 22, 26, 30, 22, 22, 24, 24, 28, 28, 26, 26, 26, 26, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28},  // Medium
-			{-1, 13, 22, 18, 26, 18, 24, 18, 22, 20, 24, 28, 26, 24, 20, 30, 24, 28, 28, 26, 30, 28, 30, 30, 30, 30, 28, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30},  // Quartile
-			{-1, 17, 28, 22, 16, 22, 28, 26, 26, 24, 28, 24, 28, 22, 24, 24, 30, 28, 28, 26, 28, 30, 24, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30},  // High
-	};
-
-	private static final byte[][] NUM_ERROR_CORRECTION_BLOCKS = {
-			// Version: (note that index 0 is for padding, and is set to an illegal value)
-			//0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40    Error correction level
-			{-1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 4,  4,  4,  4,  4,  6,  6,  6,  6,  7,  8,  8,  9,  9, 10, 12, 12, 12, 13, 14, 15, 16, 17, 18, 19, 19, 20, 21, 22, 24, 25},  // Low
-			{-1, 1, 1, 1, 2, 2, 4, 4, 4, 5, 5,  5,  8,  9,  9, 10, 10, 11, 13, 14, 16, 17, 17, 18, 20, 21, 23, 25, 26, 28, 29, 31, 33, 35, 37, 38, 40, 43, 45, 47, 49},  // Medium
-			{-1, 1, 1, 2, 2, 4, 4, 6, 6, 8, 8,  8, 10, 12, 16, 12, 17, 16, 18, 21, 20, 23, 23, 25, 27, 29, 34, 34, 35, 38, 40, 43, 45, 48, 51, 53, 56, 59, 62, 65, 68},  // Quartile
-			{-1, 1, 1, 2, 4, 4, 4, 5, 6, 8, 8, 11, 11, 16, 16, 18, 16, 19, 21, 25, 25, 25, 34, 30, 32, 35, 37, 40, 42, 45, 48, 51, 54, 57, 60, 63, 66, 70, 74, 77, 81},  // High
-	};
-
-
-
-	/*---- Public helper enumeration ----*/
-
-	/**
-	 * The error correction level in a QR Code symbol.
-	 */
-	public enum Ecc {
-		// Must be declared in ascending order of error protection
-		// so that the implicit ordinal() and values() work properly
-		/** The QR Code can tolerate about  7% erroneous codewords. */ LOW(1),
-		/** The QR Code can tolerate about 15% erroneous codewords. */ MEDIUM(0),
-		/** The QR Code can tolerate about 25% erroneous codewords. */ QUARTILE(3),
-		/** The QR Code can tolerate about 30% erroneous codewords. */ HIGH(2);
-
-		// In the range 0 to 3 (unsigned 2-bit integer).
-		final int formatBits;
-
-		// Constructor.
-		private Ecc(int fb) {
-			formatBits = fb;
-		}
-	}
 
 }
--- java/src/main/java/io/nayuki/qrcodegen/QrCodeGeneratorDemo.java
+++ java/src/main/java/io/nayuki/qrcodegen/QrCodeGeneratorDemo.java
@@ -53,7 +53,7 @@ public final class QrCodeGeneratorDemo {
 	// Creates a single QR Code, then writes it to a PNG file and an SVG file.
 	private static void doBasicDemo() throws IOException {
 		String text = "Hello, world!";          // User-supplied Unicode text
-		QrCode.Ecc errCorLvl = QrCode.Ecc.LOW;  // Error correction level
+		Ecc errCorLvl = Ecc.LOW;  // Error correction level
 		
 		QrCode qr = QrCode.encodeText(text, errCorLvl);  // Make the QR Code symbol
 		
@@ -73,15 +73,15 @@ public final class QrCodeGeneratorDemo {
 		QrCode qr;
 		
 		// Numeric mode encoding (3.33 bits per digit)
-		qr = QrCode.encodeText("314159265358979323846264338327950288419716939937510", QrCode.Ecc.MEDIUM);
+		qr = QrCode.encodeText("314159265358979323846264338327950288419716939937510", Ecc.MEDIUM);
 		writePng(qr.toImage(13, 1), "pi-digits-QR.png");
 		
 		// Alphanumeric mode encoding (5.5 bits per character)
-		qr = QrCode.encodeText("DOLLAR-AMOUNT:$39.87 PERCENTAGE:100.00% OPERATIONS:+-*/", QrCode.Ecc.HIGH);
+		qr = QrCode.encodeText("DOLLAR-AMOUNT:$39.87 PERCENTAGE:100.00% OPERATIONS:+-*/", Ecc.HIGH);
 		writePng(qr.toImage(10, 2), "alphanumeric-QR.png");
 		
 		// Unicode text as UTF-8
-		qr = QrCode.encodeText("こんにちwa、世界！ αβγδ", QrCode.Ecc.QUARTILE);
+		qr = QrCode.encodeText("こんにちwa、世界！ αβγδ", Ecc.QUARTILE);
 		writePng(qr.toImage(10, 3), "unicode-QR.png");
 		
 		// Moderately large QR Code using longer text (from Lewis Carroll's Alice in Wonderland)
@@ -92,7 +92,7 @@ public final class QrCodeGeneratorDemo {
 			+ "'without pictures or conversations?' So she was considering in her own mind (as well as she could, "
 			+ "for the hot day made her feel very sleepy and stupid), whether the pleasure of making a "
 			+ "daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly "
-			+ "a White Rabbit with pink eyes ran close by her.", QrCode.Ecc.HIGH);
+			+ "a White Rabbit with pink eyes ran close by her.", Ecc.HIGH);
 		writePng(qr.toImage(6, 10), "alice-wonderland-QR.png");
 	}
 	
@@ -105,36 +105,36 @@ public final class QrCodeGeneratorDemo {
 		// Illustration "silver"
 		String silver0 = "THE SQUARE ROOT OF 2 IS 1.";
 		String silver1 = "41421356237309504880168872420969807856967187537694807317667973799";
-		qr = QrCode.encodeText(silver0 + silver1, QrCode.Ecc.LOW);
+		qr = QrCode.encodeText(silver0 + silver1, Ecc.LOW);
 		writePng(qr.toImage(10, 3), "sqrt2-monolithic-QR.png");
 		
 		segs = Arrays.asList(
 			QrSegment.makeAlphanumeric(silver0),
 			QrSegment.makeNumeric(silver1));
-		qr = QrCode.encodeSegments(segs, QrCode.Ecc.LOW);
+		qr = QrCode.encodeSegments(segs, Ecc.LOW);
 		writePng(qr.toImage(10, 3), "sqrt2-segmented-QR.png");
 		
 		// Illustration "golden"
 		String golden0 = "Golden ratio φ = 1.";
 		String golden1 = "6180339887498948482045868343656381177203091798057628621354486227052604628189024497072072041893911374";
 		String golden2 = "......";
-		qr = QrCode.encodeText(golden0 + golden1 + golden2, QrCode.Ecc.LOW);
+		qr = QrCode.encodeText(golden0 + golden1 + golden2, Ecc.LOW);
 		writePng(qr.toImage(8, 5), "phi-monolithic-QR.png");
 		
 		segs = Arrays.asList(
 			QrSegment.makeBytes(golden0.getBytes(StandardCharsets.UTF_8)),
 			QrSegment.makeNumeric(golden1),
 			QrSegment.makeAlphanumeric(golden2));
-		qr = QrCode.encodeSegments(segs, QrCode.Ecc.LOW);
+		qr = QrCode.encodeSegments(segs, Ecc.LOW);
 		writePng(qr.toImage(8, 5), "phi-segmented-QR.png");
 		
 		// Illustration "Madoka": kanji, kana, Cyrillic, full-width Latin, Greek characters
 		String madoka = "「魔法少女まどか☆マギカ」って、　ИАИ　ｄｅｓｕ　κα？";
-		qr = QrCode.encodeText(madoka, QrCode.Ecc.LOW);
+		qr = QrCode.encodeText(madoka, Ecc.LOW);
 		writePng(qr.toImage(9, 4), "madoka-utf8-QR.png");
 		
 		segs = Arrays.asList(QrSegmentAdvanced.makeKanji(madoka));
-		qr = QrCode.encodeSegments(segs, QrCode.Ecc.LOW);
+		qr = QrCode.encodeSegments(segs, Ecc.LOW);
 		writePng(qr.toImage(9, 4), "madoka-kanji-QR.png");
 	}
 	
@@ -146,20 +146,20 @@ public final class QrCodeGeneratorDemo {
 		
 		// Project Nayuki URL
 		segs = QrSegment.makeSegments("https://www.nayuki.io/");
-		qr = QrCode.encodeSegments(segs, QrCode.Ecc.HIGH, QrCode.MIN_VERSION, QrCode.MAX_VERSION, -1, true);  // Automatic mask
+		qr = QrCode.encodeSegments(segs, Ecc.HIGH, QrCode.MIN_VERSION, QrCode.MAX_VERSION, -1, true);  // Automatic mask
 		writePng(qr.toImage(8, 6), "project-nayuki-automask-QR.png");
-		qr = QrCode.encodeSegments(segs, QrCode.Ecc.HIGH, QrCode.MIN_VERSION, QrCode.MAX_VERSION, 3, true);  // Force mask 3
+		qr = QrCode.encodeSegments(segs, Ecc.HIGH, QrCode.MIN_VERSION, QrCode.MAX_VERSION, 3, true);  // Force mask 3
 		writePng(qr.toImage(8, 6), "project-nayuki-mask3-QR.png");
 		
 		// Chinese text as UTF-8
 		segs = QrSegment.makeSegments("維基百科（Wikipedia，聆聽i/ˌwɪkᵻˈpiːdi.ə/）是一個自由內容、公開編輯且多語言的網路百科全書協作計畫");
-		qr = QrCode.encodeSegments(segs, QrCode.Ecc.MEDIUM, QrCode.MIN_VERSION, QrCode.MAX_VERSION, 0, true);  // Force mask 0
+		qr = QrCode.encodeSegments(segs, Ecc.MEDIUM, QrCode.MIN_VERSION, QrCode.MAX_VERSION, 0, true);  // Force mask 0
 		writePng(qr.toImage(10, 3), "unicode-mask0-QR.png");
-		qr = QrCode.encodeSegments(segs, QrCode.Ecc.MEDIUM, QrCode.MIN_VERSION, QrCode.MAX_VERSION, 1, true);  // Force mask 1
+		qr = QrCode.encodeSegments(segs, Ecc.MEDIUM, QrCode.MIN_VERSION, QrCode.MAX_VERSION, 1, true);  // Force mask 1
 		writePng(qr.toImage(10, 3), "unicode-mask1-QR.png");
-		qr = QrCode.encodeSegments(segs, QrCode.Ecc.MEDIUM, QrCode.MIN_VERSION, QrCode.MAX_VERSION, 5, true);  // Force mask 5
+		qr = QrCode.encodeSegments(segs, Ecc.MEDIUM, QrCode.MIN_VERSION, QrCode.MAX_VERSION, 5, true);  // Force mask 5
 		writePng(qr.toImage(10, 3), "unicode-mask5-QR.png");
-		qr = QrCode.encodeSegments(segs, QrCode.Ecc.MEDIUM, QrCode.MIN_VERSION, QrCode.MAX_VERSION, 7, true);  // Force mask 7
+		qr = QrCode.encodeSegments(segs, Ecc.MEDIUM, QrCode.MIN_VERSION, QrCode.MAX_VERSION, 7, true);  // Force mask 7
 		writePng(qr.toImage(10, 3), "unicode-mask7-QR.png");
 	}
 	
