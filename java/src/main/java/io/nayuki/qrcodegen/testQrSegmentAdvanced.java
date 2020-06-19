6db605e0fd60529078a7ccdfafb9b71c2f5dc28d
minsu4107 <32637512+minsu4107@users.noreply.github.com>
2020-06-19 22:49:50 +0900

    Checking your's min, max version whether over range in program's min, max version.

--- java/src/main/java/io/nayuki/qrcodegen/testQrSegmentAdvanced.java
+++ java/src/main/java/io/nayuki/qrcodegen/testQrSegmentAdvanced.java
@@ -9,7 +9,11 @@ public class testQrSegmentAdvanced {
 			+ "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd"
 			+ "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd"
 			+ "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd"
-			+ "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd";	
+			+ "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd";
+	
+	private static int min_version = QrCode.MIN_VERSION;
+	private static int max_version = QrCode.MAX_VERSION;
+	
 	/**
 	*Purpose: Testing when user insert over text length
 	*Input: overString, Ecc.LOW, minversion = 1, maxversion = 40
@@ -20,5 +24,36 @@ public class testQrSegmentAdvanced {
 	public void makeSegmentsOptimallyThrowtest() {
 		testing.makeSegmentsOptimally(overString, Ecc.LOW, 1, 40);
 	}
+	
+	/**
+	*Purpose: Using MCDC, Testing version set correctly.
+	*Input: nin_version, 1, 10, 27, 0
+	*Expected:
+	*	All True
+	 */
 
+	@Test
+	public void is_valid_Test() {
+		assertEquals(QrSegmentAdvanced.is_valid_version(min_version, 1),1);
+		assertEquals(QrSegmentAdvanced.is_valid_version(min_version, 10),1);
+		assertEquals(QrSegmentAdvanced.is_valid_version(min_version, 27),1);
+		assertNotEquals(QrSegmentAdvanced.is_valid_version(min_version, 0),1);
+	}
+
+	/**
+	*Purpose: Using MCDC, Testing version set correctly.
+	*Input: 1, 40, 41, 0, 3, 4
+	*Expected:
+	*	Overrange factor has return 0, So, not equal to 1
+	*	Unless it has correct range in version's range, if you insert min_version is bigger that max_version, it return 0;
+	 */
+	
+	public void not_Valid_Version_test() { 
+		assertEquals(QrSegmentAdvanced.not_Valid_Version(1, 40), 1);
+		assertNotEquals(QrSegmentAdvanced.not_Valid_Version(1, 41), 1);
+		assertNotEquals(QrSegmentAdvanced.not_Valid_Version(0, 40), 1);
+		assertNotEquals(QrSegmentAdvanced.not_Valid_Version(5, 4), 1);
+	}	
+	
+	
 }
