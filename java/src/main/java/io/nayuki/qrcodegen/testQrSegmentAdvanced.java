9205e0284f36fb934570b27eddf6e74c4319ce54
minsu4107 <32637512+minsu4107@users.noreply.github.com>
2020-06-19 22:24:15 +0900

    Using MCDC, I check flag that checking whether this version is valid.

--- java/src/main/java/io/nayuki/qrcodegen/testQrSegmentAdvanced.java
+++ java/src/main/java/io/nayuki/qrcodegen/testQrSegmentAdvanced.java
@@ -9,11 +9,7 @@ public class testQrSegmentAdvanced {
 			+ "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd"
 			+ "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd"
 			+ "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd"
-			+ "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd";
-	
-	private static int min_version = QrCode.MIN_VERSION;
-	private static int max_version = QrCode.MAX_VERSION;
-	
+			+ "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd";	
 	/**
 	*Purpose: Testing when user insert over text length
 	*Input: overString, Ecc.LOW, minversion = 1, maxversion = 40
@@ -23,21 +19,6 @@ public class testQrSegmentAdvanced {
 	@Test(expected=DataTooLongException.class)
 	public void makeSegmentsOptimallyThrowtest() {
 		testing.makeSegmentsOptimally(overString, Ecc.LOW, 1, 40);
-	}
-	
-	/**
-	*Purpose: Using MCDC, Testing version set correctly.
-	*Input: overString, Ecc.LOW, minversion = 1, maxversion = 40
-	*Expected:
-	*	Return throw
-	 */
-//			return version == minVersion || version == 10 || version == 27;
-	@Test
-	public void is_valid_Test() {
-		assertEquals(QrSegmentAdvanced.is_valid_version(min_version, 1),1);
-		assertEquals(QrSegmentAdvanced.is_valid_version(min_version, 10),1);
-		assertEquals(QrSegmentAdvanced.is_valid_version(min_version, 27),1);
-		assertNotEquals(QrSegmentAdvanced.is_valid_version(min_version, 0),1);
 	}
 
 }
