22612489576d94250b7c7edb082354a31f2a6482
Project Nayuki <me@nayuki.io>
2021-07-28 17:09:18 +0000

    Removed the test worker program in every language and the Python batch tester, because this is not core functionality and is hard to explain.

--- typescript-javascript/build.sh
+++ typescript-javascript/build.sh
@@ -23,8 +23,3 @@
 
 tsc --strict --lib DOM,DOM.Iterable,ES6 --target ES6 qrcodegen.ts qrcodegen-input-demo.ts
 tsc --strict --lib DOM,DOM.Iterable,ES6 --target ES6 qrcodegen.ts qrcodegen-output-demo.ts
-
-if [ '!' -d node_modules ]; then
-	npm install @types/node
-fi
-tsc --strict --target ES2017 --outFile qrcodegen-worker.js qrcodegen.ts qrcodegen-worker.ts
